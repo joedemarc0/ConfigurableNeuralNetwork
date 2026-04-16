845d3a5318f0bdd09b5bca470c802ddaca781d0a
demarcoj <josephfrdemarco@gmail.com>
2026-04-15 20:52:02 -0400

    Adding this for ASSERT and probably other things later

---
+++ include/utils.h
@@ -0,0 +1,22 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+#include <stdexcept>
+#include <iostream>
+
+#define ASSERT(condition, msg) \
+    if (!(condition)) { throw DetailedException(msg, __PRETTY_FUNCTION__); }
+
+
+class DetailedException : public std::exception {
+    std::string message;
+    const char* func_name;
+    public:
+        DetailedException(const char* msg, const char* func) : message(msg), func_name(func) {}
+        const char* what() const noexcept override { return message.c_str(); }
+        const char* function() const noexcept { return func_name; }
+};
+
+
+#endif // UTILS_H
