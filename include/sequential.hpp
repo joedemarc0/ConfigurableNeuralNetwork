d586691aa86467ae8d0a841ff537791f7aa25822
demarcoj <josephfrdemarco@gmail.com>
2026-05-14 12:51:31 -0400

    Finally figured out a working design that doesn't waste memory as obviously. Going to clean up this design in the next coming edits and then finally get to the actual Sequential forward and backward functions, along with GPU acceleration

--- include/sequential.hpp
+++ include/sequential.hpp
@@ -1,55 +1,52 @@
-#ifndef SEQUENTIAL_H
-#define SEQUENTIAL_H
+#ifndef SEQUENTIAL_HPP
+#define SEQUENTIAL_HPP
 
 #include "layer-config.hpp"
 
 
 class Sequential {
     public:
-        Sequential();
+        template <typename First, typename... Rest>
+        Sequential(First&& first, Rest&&... rest);
 
-        template <
-            typename... Args,
-            typename = std::enable_if_t<(std::is_base_of_v<Layer, Args>&& ...)>
-        >
-        Sequential(Args... args);
+        void compile();
     
     private:
-        bool isCompiled = false;
-        bool waitToCompile = false;
         LayerConfig layerConfig;
+        bool semiCompiled = false;
+        bool fullyCompiled = false;
+
+        template <typename T, typename... Rest>
+        void pushLayers(T&& layer, Rest&&... rest);
+        void pushLayers() {}
 
         Matrix forward(const Matrix& X);
-    
-    public:
-        void addLayer(std::unique_ptr<Layer> layer);
-        void compile();
-
-        bool checkCompiled() const { return isCompiled; }
 };
 
 
-template <
-    typename... Args,
-    typename = std::enable_if_t<(std::is_base_of_v<Layer, Args>&& ...)>
->
-Sequential::Sequential(Args... args)
-    : isCompiled(false)
-{
-    size_t layer_num = 0;
-    auto add_one = [&](auto&& layer) {
-        using T = std::decay_t<decltype(layer)>;
-        
-        if (layer_num > 0) ASSERT(layer.type() != LayerType::Input, "Only the first layer can be an input layer");
-        layerConfig.push_back(std::make_unique<T>(std::forward<decltype(layer)>(layer)));
-        ++layer_num;
-    };
+template <typename First, typename... Rest>
+Sequential::Sequential(First&& first, Rest&&... rest) {
+    static_assert(std::is_base_of_v<Layer, std::decay_t<First>>, "All arguments must derive from Layer");
 
-    (add_one(std::forward<Args>(args)), ...);
+    if constexpr (std::is_same_v<std::decay_t<First>, Input>) {
+        layerConfig.setInput(std::forward<First>(first));
+    } else {
+        layerConfig.push_back(std::forward<First>(first));
+    }
+
+    pushLayers(std::forward<Rest>(rest)...);
 }
 
+template <typename T, typename... Rest>
+void Sequential::pushLayers(T&& layer, Rest&&... rest) {
+    static_assert(std::is_base_of_v<Layer, std::decay_t<T>>, "All arguments must derive from Layer");
+    static_assert(!std::is_same_v<std::decay_t<T>, Input>, "No layer but the first can be an Input type");
+
+    layers.push_back(std::forward<T>(layer));
+    pushLayers(std::forward<Rest>(rest)...);
+}
 
 inline std::ostream& operator<<(std::ostream& os, const Sequential& model) {}
 
 
-#endif // SEQUENTIAL_H
+#endif // SEQUENTIAL_HPP
--- src/sequential.cpp
+++ src/sequential.cpp
@@ -2,65 +2,45 @@
 #include "utils.h"
 
 
-// ============================
-// Sequential Class Constructor
-// ============================
-Sequential::Sequential() : isCompiled(false) {}
+// =================================
+// Sequential Class Public Functions
+// =================================
+
+void Sequential::compile() {
+    if (semiCompiled || fullyCompiled) return;
+    ASSERT(!layerConfig.empty(), "Cannot compile Sequential with zero layers");
+
+    Layer* prev_layer = nullptr;
+    size_t expected_input_size = 0;
+    for (auto it = layerConfig.begin(); it != layerConfig.end(); ++it) {
+        Layer* curr = it.operator->();
+        ASSERT(curr->type() != LayerType::Input, "Input layer found after head node");
+
+        if (it == layerConfig.begin()) {
+            prev_layer = curr;
+            continue;
+        }
+
+        expected_input_size = prev_layer->getOutputSize();
+        curr->build(expected_input_size);
+        prev_layer = curr;
+    }
+
+    semiCompiled = true;
+}
 
 
 // ==================================
 // Sequential Class Private Functions
 // ==================================
+
 Matrix Sequential::forward(const Matrix& X) {
-    if (waitToCompile) {
-        layerConfig.insert(layerConfig.begin(), Input(X.rows()));
-        compile();
+    if (!fullyCompiled) {
+        if (!semiCompiled) compile();
+        layerConfig.input()->build(X.rows());
+        layerConfig.begin()->build(layerConfig.input()->getOutputSize());
+        fullyCompiled = true;
     }
 
-    ASSERT(isCompiled, "Network must be compiled");
-}
-
-
-// =================================
-// Sequential Class Public Functions
-// =================================
-/**
-void Sequential::addLayer(std::unique_ptr<Layer> layer) {
-    ASSERT(!isCompiled, "Cannot add layer after compiling network");
-    ASSERT(layer, "Null layer passed");
-    if (!layerConfig.empty()) ASSERT(layer->type() != LayerType::Input, "Only the first layer can be an Input layer");
-
-    layerConfig.push_back(std::move(layer));
-}
-*/
-
-void Sequential::compile() {
-    if (isCompiled) return;
-    ASSERT(!layerConfig.empty(), "Cannot compile network with 0 layers");
-
-    Layer* prev_layer = nullptr;
-    for (auto it = layerConfig.begin(); it != layerConfig.end(); ++it) {
-        Layer* curr = it.operator->();
-
-        if (it == layerConfig.begin()) {
-            if (curr->type() == LayerType::Input) {
-                curr->build(247);
-                prev_layer = curr;
-                continue;
-            } else if (curr->getInputSize() == 0) {
-                waitToCompile = true;
-                continue;
-            } else {
-                layerConfig.push_front(Input(curr->getInputSize()));
-                compile();
-            }
-        }
-
-        ASSERT(curr->type() != LayerType::Input, "Input layer must be the first layer");
-        size_t expected_input_size = prev_layer->getOutputSize();
-        curr->build(expected_input_size);
-        prev_layer = curr;
-    }
-
-    isCompiled = true;
+    // Forwarding math and stuff
 }
