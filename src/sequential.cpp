ebd0d89fc65a93c194b330b1204ad0c662074c08
demarcoj <josephfrdemarco@gmail.com>
2026-06-18 22:10:42 -0400

    Changing backward function (really just adding comments lol)

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -38,17 +38,20 @@ Matrix Sequential::forward(const Matrix& X) {
         case CompiledState::COMPILED: { break; }
     }
 
-    lastOutput = layerConfig.forward(X);
+    Matrix A = X;
+    layerConfig.forEachFromInput([&](LayerConfig::Iterator it) { A = it->forward(A); });
+    lastOutput = A;
     return lastOutput;
 }
 
-/*
 void Sequential::backward(const Matrix& y_true, double learning_rate) {
-    // size_t batch_size = y_true.cols();
+    // I know this computes dZ and that it's wrong because I'm supposed to pass in dA, but in my previous design I was supposed to pass in dZ, so it DID work
     Matrix dA = lastOutput - y_true;
+
+    // Some line like dA = Loss::compute_dA(lastOutput, y_true) probably
+    // Haven't implemented Loss though yet
     layerConfig.forEachBackwards(layerConfig.end(), layerConfig.input(), [&](LayerConfig::Iterator it) {
         dA = it->backward(dA);
         if (auto* t = dynamic_cast<Trainable*>(it.operator->())) t->update(learning_rate);
     });
 }
-*/
